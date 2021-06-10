cd4597206d75ac934c0b7af996a94d8bfe779376
Rafael Ricardo Vieceli <rafaelricardo3@gmail.com>
2021-06-10 10:57:16 +0200

    Test authenticate user

--- src/modules/users/useCases/authenticateUser/AuthenticateUserUseCase.spec.ts
+++ src/modules/users/useCases/authenticateUser/AuthenticateUserUseCase.spec.ts
@@ -3,6 +3,7 @@ import { IUsersRepository } from "../../repositories/IUsersRepository"
 import { CreateUserUseCase } from "../createUser/CreateUserUseCase"
 import { ICreateUserDTO } from "../createUser/ICreateUserDTO"
 import { AuthenticateUserUseCase } from "./AuthenticateUserUseCase"
+import { IncorrectEmailOrPasswordError } from "./IncorrectEmailOrPasswordError"
 
 
 let usersRepository: IUsersRepository
@@ -24,7 +25,6 @@ describe("Authentication User Suite", () => {
   })
 
   it("should be able to authenticate user receiving jwt token", async () => {
-
     const user = await createUserUseCase.execute(johnDoe)
 
     const authentication = await authenticateUserUseCase.execute({
@@ -33,7 +33,36 @@ describe("Authentication User Suite", () => {
     })
 
     expect(authentication).toHaveProperty("token")
-    expect(authentication.user).toMatchObject(user)
+    expect(authentication.user.id).toBe(user.id)
+  })
 
+  it("should not be able to authenticate user with wrong email", async () => {
+    expect.hasAssertions()
+
+    const user = await createUserUseCase.execute(johnDoe)
+
+    try {
+      await authenticateUserUseCase.execute({
+        email: `wrong_${johnDoe.email}`,
+        password: johnDoe.password,
+      })
+    } catch (err) {
+      expect(err).toBeInstanceOf(IncorrectEmailOrPasswordError)
+    }
+  })
+
+  it("should not be able to authenticate user with wrong password", async () => {
+    expect.hasAssertions()
+
+    const user = await createUserUseCase.execute(johnDoe)
+
+    try {
+      await authenticateUserUseCase.execute({
+        email: johnDoe.email,
+        password: `wrong_${johnDoe.password}`,
+      })
+    } catch (err) {
+      expect(err).toBeInstanceOf(IncorrectEmailOrPasswordError)
+    }
   })
 })
